9874398641a946e1e09eef9d55ed4131fe33600e
garygz <garyginzburg@hotmail.com>
2026-04-17 13:54:56 -0400

    add data ingestion endpoints

---
+++ src/main/java/com/garygz/dspdemoproject/service/impl/DataIngestionServiceImpl.java
@@ -0,0 +1,29 @@
+package com.garygz.dspdemoproject.service.impl;
+
+import com.garygz.dspdemoproject.entity.Click;
+import com.garygz.dspdemoproject.entity.Impression;
+import com.garygz.dspdemoproject.repository.ClickRepository;
+import com.garygz.dspdemoproject.repository.ImpressionRepository;
+import com.garygz.dspdemoproject.service.DataIngestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataIngestionServiceImpl implements DataIngestionService {
+
+    @Autowired
+    ImpressionRepository impressionRepository;
+
+    @Autowired
+    ClickRepository clickRepository;
+
+    @Override
+    public void addClick(Click click) {
+        clickRepository.save(click);
+    }
+
+    @Override
+    public void addImporession(Impression impression) {
+        impressionRepository.save(impression);
+    }
+}
