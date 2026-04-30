34b5d353cb7063227f37e1c071e2d9fcbc5dba98
garygz <garyginzburg@hotmail.com>
2026-04-30 10:30:57 -0400

    improve_dynamo_batching

--- src/main/java/com/garygz/dspdemoproject/service/DataIngestionService.java
+++ src/main/java/com/garygz/dspdemoproject/service/DataIngestionService.java
@@ -3,10 +3,15 @@ package com.garygz.dspdemoproject.service;
 import com.garygz.dspdemoproject.entity.Click;
 import com.garygz.dspdemoproject.entity.Impression;
 
+import java.util.List;
+
+
 public interface DataIngestionService {
 
     void addClick(Click click);
 
     void addImpression(Impression impression);
+
+    void addImpressions(List<Impression> impressions);
 
 }
